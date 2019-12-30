ecfa14a84e2c4ef3ebc8491a70caac077075a151
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-12-30 20:41:09 +0000

    adding note works

--- ToDo.js
+++ ToDo.js
@@ -1,18 +1,19 @@
 "use strict"
 
 const plus = $("#plus");
-const input = $("input");
+const input = $("#input");
 const output = $(".output");
+
 
 plus.on('click', () => {
     input.toggleClass("visible");
 });
 
-const inputValue = input.val();
 
-input.keypress( (e, inputValue) => {
+
+input.keypress( (e) => {
     if(e.which == 13){
-        output.append(inputValue);
-    }
-    
+        output.append("<p>" + input.val() + "</p>");
+        input.val("");
+    }   
 });
