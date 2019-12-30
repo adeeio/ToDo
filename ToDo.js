89e7a7a7db26955ec5e6c2d93bf0a25414b59275
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-12-30 20:05:51 +0000

    show/hide button done

--- ToDo.js
+++ ToDo.js
@@ -1,1 +1,18 @@
-console.log("connected");
+"use strict"
+
+const plus = $("#plus");
+const input = $("input");
+const output = $(".output");
+
+plus.on('click', () => {
+    input.toggleClass("visible");
+});
+
+const inputValue = input.val();
+
+input.keypress( (e, inputValue) => {
+    if(e.which == 13){
+        output.append(inputValue);
+    }
+    
+});
