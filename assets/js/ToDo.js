3fbfed0bdbdcef8ead8bc7c76a94537034eeea5d
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2020-01-08 22:28:22 +0000

    checking off and removing list done

--- assets/js/ToDo.js
+++ assets/js/ToDo.js
@@ -1,7 +1,14 @@
 'use-strict';
 
-$("#conatiner").on("click", "li", (e) => {
-    console.log(this);
-    $(this).css("color", "green");
+$("li").on("click", (e) => {
+    let element = $(e.target);
+    element.toggleClass( "checked", 100);
+});
+
+$("span").on("click", (e) => {
+    let element = $(e.target);
+    element.parent().fadeToggle(400, function () {
+        $(this).remove();
+    });
     e.stopPropagation();
 });
